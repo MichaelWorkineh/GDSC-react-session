1cf9a4e21434238cf0c53778d24181b38540f984
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-01-11 01:21:57 +0100

    day 7 and 8

---
+++ LeasonPractises/day8/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black p-4 rounded-lg shadow-md'>
+        <h1 className='text-2x1 font-bold mb-4 text-white'>Hello there govner!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
